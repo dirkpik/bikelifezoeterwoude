32935ed9e93fbe1bc07903e97391067cee0e990e
dirkpik <143648349+dirkpik@users.noreply.github.com>
2023-09-08 22:43:32 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,12 @@
+// contact.js
 
+// Functie om het contactformulier te openen
+function openContactForm() {
+    alert("Dit is het contactformulier. Vul uw gegevens in.");
+}
+
+// Voeg een event listener toe aan de knop
+document.addEventListener("DOMContentLoaded", function () {
+    const openButton = document.getElementById("openContactForm");
+    openButton.addEventListener("click", openContactForm);
+});
